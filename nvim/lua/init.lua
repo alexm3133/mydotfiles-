075955b50d0c95005fd6b086456a6774c01db686
Alex <alexandrumateiionut@gmail.com>
2024-02-29 11:46:56 +0100

    my neovim configs

--- nvim/lua/init.lua
+++
@@ -1,6 +0,0 @@
-require('settings')
-require('keymaps')
-require('plugins')
-
-vim.cmd [[autocmd BufWritePost plugins.lua PackerCompile]]
-
--- nvim/lua/lua/plugin-config/rose-pine.lua
+++
@@ -1,5 +0,0 @@
-require('rose-pine').setup({
-    dark_variant = 'moon',
-    bold = true, -- Ejemplo de configuración; ajusta según la documentación del plugin
-})
-
